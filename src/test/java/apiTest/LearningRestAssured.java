2ce7a8359f4cd6ab235b610dd5fdd658dd641050
ajit bhapse <abhapse@conga.com>
2020-12-27 19:33:58 +0530

    updated browser

--- src/test/java/apiTest/LearningRestAssured.java
+++ src/test/java/apiTest/LearningRestAssured.java
@@ -1,8 +1,5 @@
 package apiTest;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
@@ -40,6 +37,7 @@ public void postMethod() {
 			  		   .asString();
 	JsonPath path  = new JsonPath(res);
 	System.out.println(path.getString("place_id"));
+	System.out.println(System.getProperty("BROWSER"));
 }
 	@Test
 	public void getMethod() {
@@ -82,67 +80,9 @@ public void postMethod() {
 	 }
 	 
 	 
-	@Test
-	public void map() {
-			employee e1 = new employee("ajit");
-			employee e2 = new employee("ajit");
-			employee e3 = e2;
-			System.out.println(e1.equals(e2));
-			System.out.println(e1==e2);
-			System.out.println(e3.equals(e2));
-			System.out.println(e3==e2);
-			Map<employee,Integer> emp = new HashMap<employee,Integer>();
-			emp.put(e1, 123);
-			emp.put(e2, 1234);
-			System.out.println(emp.size());
-       	}
-	
-	
-	
-	class employee{
-
-		String name;
-		
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + getOuterType().hashCode();
-			result = prime * result + ((name == null) ? 0 : name.hashCode());
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			employee other = (employee) obj;
-			if (!getOuterType().equals(other.getOuterType()))
-				return false;
-			if (name == null) {
-				if (other.name != null)
-					return false;
-			} else if (!name.equals(other.name))
-				return false;
-			return true;
-		}
-
-		employee(String name){
-			this.name =  name;}
-		
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
 
 		private LearningRestAssured getOuterType() {
 			return LearningRestAssured.this;
 		}
 	}
-}
+
