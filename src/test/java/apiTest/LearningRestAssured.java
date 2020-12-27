8205f41ba85ee80409e19ee73c746c580c2058a2
ajit bhapse <abhapse@conga.com>
2020-12-27 20:17:59 +0530

    updated code for jenkins test

--- src/test/java/apiTest/LearningRestAssured.java
+++ src/test/java/apiTest/LearningRestAssured.java
@@ -4,7 +4,6 @@ import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
 public class LearningRestAssured {
 
 @Test
@@ -37,53 +36,13 @@ public void postMethod() {
 			  		   .asString();
 	JsonPath path  = new JsonPath(res);
 	System.out.println(path.getString("place_id"));
-	System.out.print("print browser "+System.getProperty("BROWSER"));
-}
-	@Test
-	public void getMethod() {
-		RestAssured.baseURI="https://rahulshettyacademy.com/";
-		String res = RestAssured.given()
-					.param("key", "qaclick123")
-					.param("place_id", "198a85b53da5b2529c4c2f861fc0ab2c")
-					.when()
-					.get("maps/api/place/get/json")
-					.then()
-					.extract()
-					.response()
-					.asString();
-		System.out.println(res);
-		System.out.print("print browser "+System.getProperty("BROWSER"));
-		/*String responseBody = res.getBody().asString();
-		System.out.println(res.getStatusLine());
-		System.out.println(res.getHeaders().getValue("Server"));
-		
-		JsonPath jpath = new JsonPath(responseBody);
-		System.out.println(jpath.getString("address"));*/
-					
+	System.out.println("print browser "+ System.getProperty("Name"));
+	if(System.getProperty("Name").equalsIgnoreCase("ajitb")) {
+		System.out.println( "this is me..");
+	}else {
+		System.out.println("this is not me..");
 	}
-
-	 @Test
-	 public void addBook() {
-		RestAssured.baseURI = "http://216.10.245.166/";
-		 Response rsp = RestAssured.given()
-		 			.header("Content-Type","application/json")
-		 			.body("{\r\n" + 
-		 					"\"name\" : \"java\",\r\n" + 
-		 					"\"isbn\" :\"123cd\",\r\n" + 
-		 					"\"aisle\" :\"2788\",\r\n" + 
-		 					"\"author\" :\"ajit bhapse\"\r\n" + 
-		 					"}").when()
-		 			        .post("Library/Addbook.php");
-		 String response = rsp.getBody().asString();
-		 JsonPath jPath = new JsonPath(response);
-		 System.out.println(jPath.getString("msg"));
-		 System.out.print("print browser "+System.getProperty("BROWSER"));
-	 }
-	 
-	 
-
-		private LearningRestAssured getOuterType() {
-			return LearningRestAssured.this;
-		}
+}
+	
 	}
 
